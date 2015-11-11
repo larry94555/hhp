9f167ea39e3a52c1c42ae10dd229b5bfdea37a78
larry94555 <larry.freeman@gmail.com>
2015-11-10 20:08:39 -0800

    some progress on ajax to update contact list.

--- HappyHourPlanner/war/js/wizard.js
+++ HappyHourPlanner/war/js/wizard.js
@@ -1,17 +1,38 @@
 $('body').on('click','button#add-contact-button',function() { 
 	// read contacts and forward
 	var rows = $("#mainTable tr:gt(0)");
+	
+	var list = new Array();
+	
 	rows.each(function(index) {
 		if ($.trim($(this).text()).length > 0) {
 			//alert($(this).text());
-			var child1 = $(this).children(":first").text();
-			var child2 = $(this).children(":nth-child(2)").text();
-			if ($.trim(child1).length > 0) {
-				alert("child1: " + child1);
+			var name = $.trim($(this).children(":first").text());
+			var email = $.trim($(this).children(":nth-child(2)").text());
+			if (name.length > 0 && email.length > 0) {
+				//alert("child1: " + child1);
+				list.push({'name': name, 'email': email});
 			}	
-			if ($.trim(child2).length > 0) {
-				alert("child2: " + child2);
-			}
+
 		}
 	});
+	
+	//alert('list.size = ' + list.length);
+	if (list.size > 0) {
+		// add to contact list and clear out (only add once)
+		var addToContacts = $.ajax({
+			url: "/contacts",
+			type: "POST",
+			data: { list: list },
+			dataType: "json"
+		});
+		addToContacts.done(function(data) {
+			// remove the values from the table
+			
+		});
+		
+		addToContacts.fail(function(jqXHR, textStatus) {
+			
+		});
+	}
 });
